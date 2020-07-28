12a571437f4a2cf37f0af9c4c768a2161f152e38
Julia Fong <Juliamzfong@gmail.com>
2020-07-28 16:43:51 -0700

    Add useStage and usePlayer logic to tetris file

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
  
 //styled components
 import { StyledTetrisWrapper, StyledTetris} from './styles/StyledTetris';
@@ -12,17 +12,27 @@ import Display from './Display';
 import StartButton from './StartButton';
 
 const Tetris = () => {
-    console.log(createStage())
+    const [dropTime, setDropTime] = useState(null);
+    const [gameOver, setGameOver] = useState(false);
+
+    const [player] = usePlayer();
+    const [stage, setStage] = useStage(player)
+
+    console.log('renderrrrrrr')
     return (
         <StyledTetrisWrapper>
             <StyledTetris>
-                <Stage  stage={createStage()} />
+                <Stage  stage={stage} />
                 <aside>
-                    <div>
-                        <Display text="Score" />
-                        <Display text="Rows" />
-                        <Display text="Level" />
-                    </div>
+                    {gameOver ? (
+                        <Display gameOver={gameOver} text="Game Over!" />
+                    ) : (
+                        <div>
+                            <Display text="Score" />
+                            <Display text="Rows" />
+                            <Display text="Level" />
+                        </div>
+                    )}
                     <StartButton />
                 </aside> 
             </StyledTetris>
