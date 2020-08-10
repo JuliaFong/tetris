4b01398485346b37ee0de93da4258c67d1f42655
Julia Fong <Juliamzfong@gmail.com>
2020-08-10 00:08:02 -0700

    Debug sttled start button file

--- src/components/styles/StyledTetris.js
+++ src/components/styles/StyledTetris.js
@@ -11,16 +11,16 @@ export const StyledTetrisWrapper = styled.div`
 `
 
 export const StyledTetris = styled.div`
-    display: flex;
-    align-items: flex-start;
-    padding: 40px;
-    margin: 0 auto;
-    max-width: 900px;
+   display: flex;
+   align-items: flex-start;
+   padding: 40px;
+   margin: 0 auto;
+   max-width: 900px;
 
-    aside {
-       width: 100%;
-       max-width: 200px;
-       display: bloack;
-       padding: 0 20px;
-    }
+   aside {
+      width: 100%;
+      max-width: 200px;
+      display: bloack;
+      padding: 0 20px;
+   }
 `
